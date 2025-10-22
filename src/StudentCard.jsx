59eec08bcff57281983660c995f78b8efb6e7393
Sumit Maurya <hsumit263@gmail.com>
2025-10-22 15:31:24 +0530

    show/hide details

--- src/StudentCard.jsx
+++ src/StudentCard.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react"
 
-export default function StudentCard({name, course, image, description}){
+export default function StudentCard({ name, course, image, description }) {
+
+    const [visible, setVisible] = useState(false);
 
     return (
         <div className="student-card" >
             <img src={image} alt="no image" />
-            <h3>{name}</h3>
-            <h4>{course}</h4>
-            <p>{description}</p>
+            {visible && (
+                <>
+                    <h3>{name}</h3>
+                    <h4>{course}</h4>
+                    <p>{description}</p>
+                </>
+            )}
+
+            <button onClick={() => setVisible(!visible)}>{visible? "Hide" : "Show"} Details</button>
+
         </div>
     )
 
